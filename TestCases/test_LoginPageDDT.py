652559055267254dab772abc1089f2bc367aeafa
zhangli <997750687@qq.com>
2024-12-30 22:04:15 -0500

    modify read data from s3

--- TestCases/test_LoginPageDDT.py
+++ TestCases/test_LoginPageDDT.py
@@ -12,13 +12,16 @@ from utilities.readconfig import readConfig
 import pytest
 import time
 from utilities import ExcelUtility
-
+from utilities.readconfig import readConfig
+from utilities.s3_reader import *
 @pytest.mark.usefixtures("setup_teardown")
 class TestLogin:
     logger = LogGen.loggen()
 
+    @pytest.marker.pc
     def test_login_ddt(self):
         self.path = ".//TestData//LoginInfo.xlsx"
+
         self.rows = ExcelUtility.getRowCount(self.path,'LoginInfo')
         self.columns = ExcelUtility.getColumnCount(self.path,'LoginInfo')
 
@@ -66,6 +69,59 @@ class TestLogin:
         else:
             self.logger.info('************Login DDT Test Fail***********')
             assert False
+
+    @pytest.marker.ec2
+    def test_login_ddt(self):
+        self.bucket_name = readConfig('s3 info', 'bucketName')
+        self.file_key = readConfig('s3 info', 'fileKey')
+        self.workbook = get_data_s3(self.bucket_name,self.file_key)
+        self.rows = getRowCount_froms3(self.workbook,'LoginInfo')
+        self.columns = getColumnCount_froms3(self.workbook, 'LoginInfo')
+
+        homepage = HomePage(self.driver)  # return driver after invoke fixtures in conftest.py
+        time.sleep(2)
+
+        self.logger.info('*****************Start*********************')
+        self.logger.info('***********Verifying login page ddt***********')
+        test_status = []  # record the test results
+        for i in range(2, self.rows + 1):
+            self.email = readData_froms3(self.workbook, 'LoginInfo', i, 1)
+            self.password = readData_froms3(self.workbook,'LoginInfo', i, 2)
+            self.exp = readData_froms3(self.workbook,'LoginInfo', i, 3)
+            self.alertinfo = readData_froms3(self.workbook,'LoginInfo', i, 4)
+            homepage.click_on_my_account_drop_menu()
+            homepage.select_login_option()
+            loginpage = LoginPage(self.driver)
+            loginpage.enter_email_address(self.email)
+            loginpage.enter_password(self.password)
+            loginpage.click_on_login_button()
+            time.sleep(2)
+
+            if self.exp == 'Pass':
+                accountpage = AccountPage(self.driver)
+                if accountpage.display_status_of_edit_your_account_information_option():
+                    self.logger.info("*******use correct username and password ddt test login page  passed********")
+                    test_status.append('Pass')
+                    accountpage.logout_click()
+                else:
+                    self.logger.info("*******use correct username and password ddt test login page  failed********")
+                    test_status.append('Fail')
+            elif self.exp == 'Fail':
+                expected_warning_message = 'Warning: No match for E-Mail Address and/or Password.'
+                if loginpage.retrieve_warning_message() == expected_warning_message:
+                    self.logger.info("*******use invalid username or invalid pass test login page passed********")
+                    test_status.append('Pass')
+                else:
+                    self.logger.info("*******use invalid username test login page failed********")
+                    test_status.append('Fail')
+
+        if 'Fail' not in test_status:
+            self.logger.info('************Login DDT Test Passed***********')
+            assert True
+        else:
+            self.logger.info('************Login DDT Test Fail***********')
+            assert False
+
 
 
 
--- utilities/s3_reader.py
+++ utilities/s3_reader.py
@@ -1,16 +1,31 @@
 import boto3
-import json
+from openpyxl import load_workbook
+from io import BytesIO
 
 def get_data_s3(bucket_name,file_key):
     s3 = boto3.client('s3')
     try:
         response = s3.get_object(Bucket=bucket_name, Key=file_key)
-        data = response['Body'].read().decode('utf-8')
-        return json.loads(data)
+        file_content = response['Body'].read()
+        workbook = load_workbook(filename=BytesIO(file_content))
+        return workbook
     except Exception as e:
         print(f"Failed to load data: {e}")
         return None
 
+def getRowCount_froms3(workbook,sheetname):
+    sheet = workbook[sheetname]
+    return sheet.max_row
+
+def getColumnCount_froms3(workbook,sheetname):
+    #workbook = get_data_s3(bucket_name,file_key)
+    sheet = workbook[sheetname]
+    return sheet.max_column
+
+def readData_froms3(workbook,sheetname,rownum,columnno):
+    #workbook = get_data_s3(bucket_name,file_key)
+    sheet = workbook[sheetname]
+    return sheet.cell(row=rownum,column=columnno).value
 
 if __name__=='__main__':
     print(get_data_s3('opencart-login-testdata','LoginInfo.json'))
